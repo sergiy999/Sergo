e46f368b7bcfe82ab3cd7c2424041ac104710d8b
Sergo <guchok1971@gmail.com>
2022-03-17 14:07:06 +0200

    eto 1 commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+// var number =5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj ={};
+ 
+// console.log(4/8);
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+let persone = {
+    name: "yarik",
+    age: 25,
+    isMarried:false
+};
+
+console.log(persone["name"]);
+ 
+// arr = [ 'plum.png','orange.jpg'];
+
+// console.log(arr[0]);
+// let answer =confirm("Are you here?");
+
+// console .log(answer);
+
+// 
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%5);
+// console.log("2" === 2);
+
+// let ihCheced = true,
+//     ihclose = false;
+
+// console.log(ihCheced || ihCheced);
+
+// let y = 1; let x = y = 2; alert(x);
+
+// alert( 0 || "" || 2 || undefined || true || falsе );
